0b9a5cc10b2847fd6ccd792985c70c0c33b2aea5
jahidulkanchan <mdj524156@gmail.com>
2025-03-01 23:13:49 +0600

    shoping part added

--- client-site/src/components/navbar/Navbar.jsx
+++ client-site/src/components/navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { FaUser } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaRegHeart, FaUser } from "react-icons/fa";
 import { FaBarsStaggered, FaRegCircleUser } from "react-icons/fa6";
-import { LiaSearchSolid } from "react-icons/lia";
 import { Link } from "react-router-dom";
 import SearchBar from "../../shared/SearchBar";
+import { RxCross2 } from "react-icons/rx";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+import { IoMdHeartEmpty } from "react-icons/io";
 
 const Navbar = () => {
-  const [ishidden,setIshidden] = useState(true)
+  const [isHidden, setIsHidden] = useState(true);
   const [show, setShow] = useState(true);
 
-  console.log(ishidden)
+  console.log(isHidden)
 
   let lastScrollY = 0;
   useEffect(() => {
@@ -35,7 +37,10 @@ const Navbar = () => {
          <Link to='/'> <img className="logo w-[80px] md:w-[100px]" src="/logo.png" alt="Logo" /></Link>
         </div>
         {/* our menu items */}
-        <div  className={`${ishidden? '-right-[200px]' : 'right-[0px]'} absolute duration-200 md:static top-[50px] bg-white md:bg-transparent  px-5 md:px-0 py-8 md:py-0`}>
+        <div  className={`${isHidden? '-right-[200px]' : 'right-[0px]'} absolute duration-200 md:static top-[50px] bg-white md:bg-transparent  px-5 md:px-0 py-8 md:py-0`}>
+        <div className="relative -right-2 -top-5 block md:hidden">
+            <div onClick={()=>setIsHidden(!isHidden)} className="cancel-btn cursor-pointer text-2xl w-fit absolute right-0 top-0"><RxCross2 /></div>
+            </div>
           <ul className="flex flex-col md:flex-row md:items-center justify-center gap-5 md:gap-8">
             <li className="relative group w-fit"><span className="absolute w-[8px] group-hover:w-full duration-200 h-[2px] bg-amber-400 bottom-0"></span><Link to='/food'>Food</Link></li>
             <li className="relative group w-fit"><span className="absolute w-[8px] group-hover:w-full duration-200 h-[2px] bg-amber-400 bottom-0"></span><Link to='/gadget'>Gadget</Link> </li>
@@ -44,20 +49,27 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="user-info hidden md:block">
-        <Link to='/signin' className="px-3 bg-white rounded flex items-center gap-1.5 border-amber-400 text-primary py-1 border">
-          <FaUser /> Sign In
+        <Link to='/signin' className="px-3 bg-white rounded flex items-center gap-1.5 border-amber-400 py-1 border">
+          <FaUser className="text-amber-400" /> <span>Sign In</span>
         </Link>
         </div>
         <div className="flex md:hidden items-center gap-4">
-          <div className="text-2xl text-amber-400 md:hidden"><FaRegCircleUser /></div>
+          <div className="relative md:hidden">
+            <Link to='/signin'><span className="group text-2xl text-amber-500"><FaRegCircleUser /></span></Link>
+          </div>
         {/* Hambarger icon */}
-        <div onClick={()=>setIshidden(!ishidden)} className={`${ishidden? '' : 'text-amber-400'} bar-icon cursor-pointer text-xl`}>
+        <div onClick={()=>setIsHidden(!isHidden)} className={`${isHidden? '' : 'text-amber-500'} bar-icon cursor-pointer text-xl`}>
         <FaBarsStaggered />
         </div>
         </div>
       </div>
-      <div className="min-h-[50px] flex justify-center items-center bg-amber-300">
-      <SearchBar/>
+      <div className="min-h-[50px] container mx-auto px-1.5 md:px-5 flex justify-between items-center bg-amber-400">
+        <div className="hidden md:block"></div>
+        <SearchBar/>
+        <div className="shopping-info flex items-center gap-2 md:gap-3">
+          <Link to='/' className="text-2xl mt-1"><IoMdHeartEmpty /></Link>
+          <Link to='/' className="text-2xl"><LiaShoppingBagSolid /></Link>
+        </div>
       </div>
     </nav>
    </>
--- client-site/src/shared/SearchBar.jsx
+++ client-site/src/shared/SearchBar.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { LiaSearchSolid } from 'react-icons/lia';
+import { ImSearch } from 'react-icons/im';
+
 const SearchBar = () => {
   const [query, setQuery] = useState("");
   return (
     <>
-      <div className="search-bar w-[250px] md:w-[350px] bg-white border-amber-400 rounded px-4 flex justify-center items-center gap-1.5 border">
-          <input
+      <div className="search-bar max-w-[280px] md:min-w-[400px] bg-white border-amber-400 rounded px-4 flex justify-between items-center gap-1.5 border">
+          <input 
             type="text"
             placeholder="Search your products"
             className="py-1 md:w-[290px] outline-none"
             value={query} 
             onChange={(e) => setQuery(e.target.value)}
           />
-          <LiaSearchSolid className="font-medium text-primary text-xl" />
+          <span className="text-green-500 text-lg"><ImSearch /></span>
       </div>
     </>
   );
